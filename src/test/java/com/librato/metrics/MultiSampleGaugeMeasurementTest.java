c0f2983cb3215c6a5fc1cef1c3d3dae80c9056ec
unknown <BGehrels@ISDEBLNWS086.iscout.local>
2012-08-08 18:12:54 +0200

    fixed indention

--- src/test/java/com/librato/metrics/MultiSampleGaugeMeasurementTest.java
+++ src/test/java/com/librato/metrics/MultiSampleGaugeMeasurementTest.java
@@ -10,35 +10,35 @@ import org.junit.Test;
  * Verify that all the advanced details are filled in that are available
  */
 public class MultiSampleGaugeMeasurementTest {
-  @Test(expected = IllegalArgumentException.class)
-  public void doesNotAcceptNaNAsSum() {
-    new MultiSampleGaugeMeasurement("testGauge", 12L, Double.NaN, 2, 2, 2);
-  }
+    @Test(expected = IllegalArgumentException.class)
+    public void doesNotAcceptNaNAsSum() {
+        new MultiSampleGaugeMeasurement("testGauge", 12L, Double.NaN, 2, 2, 2);
+    }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void doesNotAcceptPosInfAsSum() {
-    new MultiSampleGaugeMeasurement("testGauge", 12L, Double.POSITIVE_INFINITY, 2, 2, 2);
-  }
+    @Test(expected = IllegalArgumentException.class)
+    public void doesNotAcceptPosInfAsSum() {
+        new MultiSampleGaugeMeasurement("testGauge", 12L, Double.POSITIVE_INFINITY, 2, 2, 2);
+    }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void doesNotAcceptNegInfAsSum() {
-    new MultiSampleGaugeMeasurement("testGauge", 12L, Double.NEGATIVE_INFINITY, 2, 2, 2);
-  }
+    @Test(expected = IllegalArgumentException.class)
+    public void doesNotAcceptNegInfAsSum() {
+        new MultiSampleGaugeMeasurement("testGauge", 12L, Double.NEGATIVE_INFINITY, 2, 2, 2);
+    }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void doesNotAcceptNonNumericValuesAsMax() {
-    new MultiSampleGaugeMeasurement("testGauge", 12L, 2, Double.NEGATIVE_INFINITY, 2, 2);
-  }
+    @Test(expected = IllegalArgumentException.class)
+    public void doesNotAcceptNonNumericValuesAsMax() {
+        new MultiSampleGaugeMeasurement("testGauge", 12L, 2, Double.NEGATIVE_INFINITY, 2, 2);
+    }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void doesNotAcceptNonNumericValuesAsMin() {
-    new MultiSampleGaugeMeasurement("testGauge", 12L, 2, 2, Double.POSITIVE_INFINITY, 2);
-  }
+    @Test(expected = IllegalArgumentException.class)
+    public void doesNotAcceptNonNumericValuesAsMin() {
+        new MultiSampleGaugeMeasurement("testGauge", 12L, 2, 2, Double.POSITIVE_INFINITY, 2);
+    }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void doesNotAcceptNonNumericValuesAsSumSquares() {
-    new MultiSampleGaugeMeasurement("testGauge", 12L, 2, 2, 2, Double.NaN);
-  }
+    @Test(expected = IllegalArgumentException.class)
+    public void doesNotAcceptNonNumericValuesAsSumSquares() {
+        new MultiSampleGaugeMeasurement("testGauge", 12L, 2, 2, 2, Double.NaN);
+    }
 
 
 }

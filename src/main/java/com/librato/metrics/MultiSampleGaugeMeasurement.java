7db14bc5bd88d65fd16cdc86468f5d4f9f7f5cbd
unknown <BGehrels@ISDEBLNWS086.iscout.local>
2012-08-08 18:12:02 +0200

    Enforcing compatibility with the librato REST Api
    I have had a few problems when using metrics-librato, which inherits the LibratoBatch class. It sometimes pushes NaN values to in the MultiSampleGaugeMeasurement, wich will lead to error 400 when beeing POSTed to the librato API. Even if the real error is located in the metrics-librato package, this class should none the less check for invalid input. The same holds true for the count field: The REST API will not accept fractional counts - thats why i changed it to be a Long.

--- src/main/java/com/librato/metrics/MultiSampleGaugeMeasurement.java
+++ src/main/java/com/librato/metrics/MultiSampleGaugeMeasurement.java
@@ -3,29 +3,34 @@ package com.librato.metrics;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Double.isInfinite;
+import static java.lang.Double.isNaN;
+
+
 /**
  * User: mihasya
  * Date: 6/17/12
  * Time: 10:06 PM
  * a class for representing a gauge reading that might come from multiple samples
- *
+ * <p/>
  * See http://dev.librato.com/v1/post/metrics for why some fields are optional
  */
 public class MultiSampleGaugeMeasurement implements Measurement {
     private final String name;
-    private final Number count;
+    private final Long count;
     private final Number sum;
     private final Number max;
     private final Number min;
     private final Number sum_squares;
 
-    public MultiSampleGaugeMeasurement(String name, Number count, Number sum, Number max, Number min, Number sum_squares) {
+    public MultiSampleGaugeMeasurement(String name, Long count, Number sum, Number max, Number min,
+                                       Number sum_squares) {
         this.name = name;
         this.count = count;
-        this.sum = sum;
-        this.max = max;
-        this.min = min;
-        this.sum_squares = sum_squares;
+        this.sum = numeric("sum", sum);
+        this.max = numeric("max", max);
+        this.min = numeric("max", min);
+        this.sum_squares = numeric("max", sum_squares);
     }
 
     @Override
@@ -48,5 +53,17 @@ public class MultiSampleGaugeMeasurement implements Measurement {
             result.put("sum_squares", sum_squares);
         }
         return result;
+    }
+
+    private Number numeric(String name, Number number) {
+        if (number == null) {
+            return null;
+        }
+
+        if (isNaN(number.doubleValue()) || isInfinite(number.doubleValue())) {
+            throw new IllegalArgumentException(number + " is not a numeric value for " + name);
+        }
+
+        return number;
     }
 }

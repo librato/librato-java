be85e3b0c89846b990340d36bc57441ddcbd9b6a
Brian Renzenbrink <brenzenb@brenzenbs-MacBook-Air.local>
2014-06-06 18:25:45 -0400

    Implement ability to define period for counter or gauge measurement

--- src/main/java/com/librato/metrics/MultiSampleGaugeMeasurement.java
+++ src/main/java/com/librato/metrics/MultiSampleGaugeMeasurement.java
@@ -12,6 +12,7 @@ import static com.librato.metrics.Preconditions.checkNumeric;
  * See http://dev.librato.com/v1/post/metrics for why some fields are optional
  */
 public class MultiSampleGaugeMeasurement implements Measurement {
+    private final Long period;
     private final String source;
     private final String name;
     private final Long count;
@@ -36,11 +37,23 @@ public class MultiSampleGaugeMeasurement implements Measurement {
                                        Number max,
                                        Number min,
                                        Number sumSquares) {
+        this(source, null, name, count, sum, max, min, sumSquares);
+    }
+
+    public MultiSampleGaugeMeasurement(String source,
+                                       Long period,
+                                       String name,
+                                       Long count,
+                                       Number sum,
+                                       Number max,
+                                       Number min,
+                                       Number sumSquares) {
         try {
             if (count == null || count == 0) {
                 throw new IllegalArgumentException("The Librato API requires the count to be > 0 for complex metrics. See http://dev.librato.com/v1/post/metrics");
             }
             this.source = source;
+            this.period = checkNotNull(period);
             this.name = checkNotNull(name);
             this.count = count;
             this.sum = checkNumeric(sum);
@@ -60,8 +73,11 @@ public class MultiSampleGaugeMeasurement implements Measurement {
         return name;
     }
 
+    public Long getPeriod() { return period; }
+
     public Map<String, Number> toMap() {
         final Map<String, Number> result = new HashMap<String, Number>(5);
+        result.put("period", period);
         result.put("count", count);
         result.put("sum", sum);
         if (max != null) {
